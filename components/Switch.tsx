d01e85538fefd7812ddb17672cfbe0c0be885647
eunjin0212 <eunjin0212@gmail.com>
2022-12-28 22:48:27 +0900

    🎨 added 'mode' in localStorage
    다크모드 유지 기능 추가

--- components/Switch.tsx
+++ components/Switch.tsx
@@ -3,13 +3,18 @@ import { ChangeEvent, useEffect, useState } from 'react';
 import { CustomizedSwich } from 'styles/components/Switch.style';
 
 const ThemeMode = (props: SwitchProps) => {
+	console.log();
 	const [mode, setMode] = useState<boolean>(false);
+  useEffect(() => {
+    setMode(Boolean(window.localStorage.getItem('mode') === 'true'))
+  }, [])
 	const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
 		setMode(event.target.checked);
+		window.localStorage.setItem('mode', JSON.stringify(event.target.checked));
 	};
-  useEffect(() => {
-    document.body.dataset.theme = mode ? 'dark' : 'light';
-  }, [mode])
+	useEffect(() => {
+		document.body.dataset.theme = mode ? 'dark' : 'light';
+	}, [mode]);
 	return (
 		<CustomizedSwich
 			checked={mode}
